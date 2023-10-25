1a3086d2ecf832a67c11ee820694d1a0edfad931
Dezare Chaney <dchaney3@mkc02gd5c3md6r.humad.com>
2023-10-25 09:50:03 -0400

    Java Array Activity

---
+++ arrayact1.js
@@ -0,0 +1,41 @@
+
+
+//1. Console log the length of the array, console log the 3rd value of the array, console log the 5th vaulue of the array
+let myAlphabet = ['A', 'B', 'C', 'D','E','F', 'G'];
+console.log(myAlphabet[3],[5]);
+
+
+//2. Declare and initialize an array called Planets with 8 string values
+*let myPlants = ["Flowering Plant", "Shrub", "Liverwart", "Fern", "Lucky Bamboo", "Pothos", "Snake Plant", "Cactus"]*
+
+// 3. For each item in this array console.log the item
+let furniture = ['Table', 'Chairs','Couch'];
+
+for (let i =; i < furniture.length; i++){
+    console.log(furniture{i});
+}
+
+//4. Loop over the array and if the value is "Math" console log "yuck"
+let student1Courses = ['Math', 'English', 'Programming'];
+
+for()
+
+//5.  Create an array of 4 fruits, then add 2 fruits (kiwi, grapes) to the end of the array using the method learned. Display the result in the console.
+ let fruits = ["apple", "orange", "pear", "banana"]   
+(Array becomes apple, orange, pear, banana, kiwi, grapes)
+
+//6. Remove the last value (grapes) from the array in #5. Display the result in the console.
+(Array becomes apple, orange, pear, banana, kiwi)
+
+//7. Remove the first fruit (apple) from the array in #5. Display the result in the console.
+(Array becomes orange, pear, banana, kiwi)
+
+//8.  Add a fruit (dragonfruit) to the beginning of the array in #5. Display the result in the console.
+(Array becomes dragonfruit, orange, pear, banana, kiwi)
+
+//9.   Change the fruit 'orange' to be the fruit 'persimmon', and display the result in the console.
+(Array becomes dragonfruit, persimmon, pear, banana, kiwi)
+
+//10.  In our console, display the length of our fruit array from #9.  The output should read "My array has____items."
+
+Bonus:  Add a fruit (pomegranate) in the second location of your array. Display the result in the console. (Display should now read dragonfruit, pomegranate, persimmon, pear, banana, kiwi.)
